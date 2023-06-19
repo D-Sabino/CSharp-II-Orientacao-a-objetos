dedf93c3e2ca9b84babaa7a3fd77bb9bdb7d5bff
Daniel Sabino <dannisabino17@gmail.com>
2023-06-19 06:28:40 -0300

    Aula 01: Encapsulamento e Modificadores de Acesso

---
+++ CaixaEletronico/CaixaEletronico/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Cliente
+    {
+        public string nome;
+        public string rgTitular;
+        public string cpf;
+        public string endereco;
+        public int idade;
+
+        public bool EhMaiorDeIdade()
+        {
+            if (this.idade >= 18)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ CaixaEletronico/CaixaEletronico/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+        private int agencia;
+
+        public Cliente cliente;
+
+        public bool Saca(double valorASerSacado)
+        {
+            if (this.Saldo >= valorASerSacado && valorASerSacado >= 0)
+            {
+                if (this.cliente.EhMaiorDeIdade())
+                {
+                    this.Saldo -= valorASerSacado;
+                    return true;
+                }
+                else if (valorASerSacado <= 200)
+                {
+                    this.Saldo -= valorASerSacado;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        public void Deposita(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public void Transfere(double valor, Conta destino)
+        {
+            this.Saca(valor);
+            destino.Deposita(valor);
+        }
+    }
+}
